295c895262bba0727e59c010a75a41995f631ebd
Issarapong Poesua <dekablade01@gmail.com>
2017-06-19 09:41:25 +0700

    colored profit

--- eMiner/UIViewControllers/ProfitCalculatorViewController.swift
+++ eMiner/UIViewControllers/ProfitCalculatorViewController.swift
@@ -7,38 +7,51 @@
 //
 
 import UIKit
+import Material
 
 class ProfitCalculatorViewController: BlueNavigationBarViewController
 {
 
-    @IBOutlet weak var minerNameTextLabel: UITextField!
     
+    @IBOutlet weak var minerNameTextLabel: UILabel!
     @IBOutlet weak var numberOfMinersTextLabel: UITextField!
-    
     var hardwares: [HardwareModel] = []
     
     var minerName: String {
         get { return minerNameTextLabel.text ?? "" }
         set { minerNameTextLabel.text = newValue }
     }
-    var numberOfMiners: Double {
-        get { return  Double(numberOfMinersTextLabel.text!) ?? 0.0}
+    var numberOfMiners: Int {
+        get { return  Int(numberOfMinersTextLabel.text!) ?? 0}
         set { numberOfMinersTextLabel.text = "\(newValue)" }
     }
+    @IBOutlet weak var minerLabelContainer: UIView!
+    @IBOutlet weak var minerNameContainer: UIView!
+    
+    @IBOutlet weak var numbersLabelContainer: UIView!
+    @IBOutlet weak var numberContainer: UIView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        setupColors()
     }
     
     func getMiners()
     {
         self.startActivityIndicator()
-        
         RemoteFactory
             .remoteFactory
             .remoteHardware
             .getHardwareList() { self.hardwares = $0.0  }
+    }
+    func setupColors()
+    {
+        minerLabelContainer.backgroundColor = Color.blue.darken2
+        minerNameContainer.backgroundColor = Color.blue.darken1
+        
+        numbersLabelContainer.backgroundColor = Color.blue.darken1
+        numberContainer.backgroundColor = Color.blue.base
     }
 
 
